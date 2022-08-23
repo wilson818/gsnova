6d81e7417c705ebfab45dfb023deeeb82668a165
wilson818 <32030660+wilson818@users.noreply.github.com>
2022-08-24 02:45:36 +0800

    Update remote.go

--- common/channel/kcp/remote.go
+++ common/channel/kcp/remote.go
@@ -2,9 +2,9 @@ package kcp
 
 import (
 	kcp "github.com/xtaci/kcp-go"
-	"github.com/yinqiwen/gsnova/common/channel"
-	"github.com/yinqiwen/gsnova/common/logger"
-	"github.com/yinqiwen/gsnova/common/mux"
+	"github.com/wilson818/gsnova/common/channel"
+	"github.com/wilson818/gsnova/common/logger"
+	"github.com/wilson818/gsnova/common/mux"
 	"github.com/yinqiwen/pmux"
 )
 
