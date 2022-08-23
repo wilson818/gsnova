015853ce974d5a0c9e835af01604aa1a1b7887de
wilson818 <32030660+wilson818@users.noreply.github.com>
2022-08-24 05:02:43 +0800

    Update local.go

--- common/channel/quic/local.go
+++ common/channel/quic/local.go
@@ -49,7 +49,7 @@ func (tc *QUICProxy) CreateMuxSession(server string, conf *channel.ProxyChannelC
 		return nil, err
 	}
 	quicConfig := &quic.Config{
-		KeepAlive: true,
+		config.KeepAlive: true,
 	}
 	quicSession, err = quic.Dial(udpConn, udpAddr, hostport, &tls.Config{InsecureSkipVerify: true}, quicConfig)
 
