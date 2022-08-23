9e54619a9a4805b7737b65f7ce61e0f2e07df67c
wilson818 <32030660+wilson818@users.noreply.github.com>
2022-08-24 02:43:01 +0800

    Update common.go

--- common/channel/common/common.go
+++ common/channel/common/common.go
@@ -1,12 +1,12 @@
 package common
 
 import (
-	_ "github.com/yinqiwen/gsnova/common/channel/direct"
-	_ "github.com/yinqiwen/gsnova/common/channel/http"
-	_ "github.com/yinqiwen/gsnova/common/channel/http2"
-	_ "github.com/yinqiwen/gsnova/common/channel/kcp"
-	_ "github.com/yinqiwen/gsnova/common/channel/quic"
-	_ "github.com/yinqiwen/gsnova/common/channel/ssh"
-	_ "github.com/yinqiwen/gsnova/common/channel/tcp"
-	_ "github.com/yinqiwen/gsnova/common/channel/websocket"
+	_ "github.com/wilson818/gsnova/common/channel/direct"
+	_ "github.com/wilson818/gsnova/common/channel/http"
+	_ "github.com/wilson818/gsnova/common/channel/http2"
+	_ "github.com/wilson818/gsnova/common/channel/kcp"
+	_ "github.com/wilson818/gsnova/common/channel/quic"
+	_ "github.com/wilson818/gsnova/common/channel/ssh"
+	_ "github.com/wilson818/gsnova/common/channel/tcp"
+	_ "github.com/wilson818/gsnova/common/channel/websocket"
 )
