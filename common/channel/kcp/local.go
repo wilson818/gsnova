b74bea18b45d22eeb65188dab76561133d3778b1
wilson818 <32030660+wilson818@users.noreply.github.com>
2022-08-24 02:44:51 +0800

    Update local.go

--- common/channel/kcp/local.go
+++ common/channel/kcp/local.go
@@ -6,11 +6,11 @@ import (
 	"net/url"
 
 	kcp "github.com/xtaci/kcp-go"
-	"github.com/yinqiwen/gsnova/common/channel"
-	"github.com/yinqiwen/gsnova/common/dns"
-	"github.com/yinqiwen/gsnova/common/logger"
-	"github.com/yinqiwen/gsnova/common/mux"
-	"github.com/yinqiwen/gsnova/common/netx"
+	"github.com/wilson818/gsnova/common/channel"
+	"github.com/wilson818/gsnova/common/dns"
+	"github.com/wilson818/gsnova/common/logger"
+	"github.com/wilson818/gsnova/common/mux"
+	"github.com/wilson818/gsnova/common/netx"
 	"github.com/yinqiwen/pmux"
 )
 
