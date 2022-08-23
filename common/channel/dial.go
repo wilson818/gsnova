752f5a1f52507eb63bc59cb46750dbfb3a7a498a
wilson818 <32030660+wilson818@users.noreply.github.com>
2022-08-24 02:39:45 +0800

    Update dial.go

--- common/channel/dial.go
+++ common/channel/dial.go
@@ -10,12 +10,12 @@ import (
 	"sync"
 	"time"
 
-	"github.com/yinqiwen/gsnova/common/dns"
-	"github.com/yinqiwen/gsnova/common/helper"
-	"github.com/yinqiwen/gsnova/common/hosts"
-	"github.com/yinqiwen/gsnova/common/logger"
-	"github.com/yinqiwen/gsnova/common/netx"
-	"github.com/yinqiwen/gsnova/common/protector"
+	"github.com/wilson818/gsnova/common/dns"
+	"github.com/wilson818/gsnova/common/helper"
+	"github.com/wilson818/gsnova/common/hosts"
+	"github.com/wilson818/gsnova/common/logger"
+	"github.com/wilson818/gsnova/common/netx"
+	"github.com/wilson818/gsnova/common/protector"
 )
 
 func NewTLSConfig(conf *ProxyChannelConfig) *tls.Config {
